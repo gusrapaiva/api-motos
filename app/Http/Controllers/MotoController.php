19a32e306ac11611039c6d473568758a73728ddf
gusrapaiva <gustavorapaiva@gmail.com>
2024-05-14 10:49:40 -0300

    crud funcionando

--- app/Http/Controllers/MotoController.php
+++ app/Http/Controllers/MotoController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\moto;
+use GuzzleHttp\Psr7\Response as Psr7Response;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\RedirectResponse;
 
@@ -52,24 +53,64 @@ class MotoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(moto $moto)
+    public function show(string $id)
     {
-        //
+        $moto = Moto::find($id);
+
+        if($moto)
+        {
+            return "Moto localizada" . $moto . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return "Moto não localizada" . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, moto $moto)
+    public function update(Request $request, string $id)
     {
-        //
+        $motos = $request->all();
+
+        $validarDados = Validator::make($motos, [
+            'marca' => 'required',
+            'modelo' => 'required',
+            'cor' => 'required',
+            'ano' => 'required',
+        ]);
+
+        if($validarDados->fails())
+        {
+            return 'Dados inválidos' . $validarDados->error(true) . 500;
+        } 
+
+        $moto = Moto::find($id);
+        $moto->marca = $motos['marca'];
+        $moto->modelo = $motos['modelo'];
+        $moto->cor = $motos['cor'];
+        $moto->ano = $motos['ano'];
+
+        if($moto->save())
+        {
+            return "Dados atualizados com sucesso." . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return "Dados não atualizados." . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(moto $moto)
+    public function destroy(string $id)
     {
-        //
+        $motos = Moto::find($id);
+
+        if($motos->delete())
+        {
+            return "Dados deletados com sucesso." . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return "Falha ao deletar dados." . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
+            
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,7 @@ use App\Http\Controllers\MotoController;
 
 route::get('/', function() {return response()->json(['Sucesso' => true]);});
 route::get('/motos', [MotoController::class, 'index']);
-route::post('/motos', [MotoController::class, 'store']);
+route::post('/store-motos', [MotoController::class, 'store']);
+route::get('/find-motos/{id}', [MotoController::class, 'show']);
+route::put('/update-motos/{id}', [MotoController::class, 'update']);
+route::delete('/destroy-motos/{id}', [MotoController::class, 'destroy']);
